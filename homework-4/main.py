10227c7c749821cd5937ba2eddaaf087b8a18f73
Vladimir Nikitin <vanjilbroolkyn@gmail.com>
2024-01-29 00:33:11 -0500

    не могу исправить ошибку

--- homework-4/main.py
+++ homework-4/main.py
@@ -1,10 +1,12 @@
 from src.item import Item
-from phone import Phone
+from src.phone import Phone
 
 if __name__ == '__main__':
-
-    # смартфон iPhone 14, цена 120_000, количество товара 5, симкарт 2
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    item1 = Item("Смартфон", 10000, 20)
+
+    result = item1 + phone1
+    print(result.quantity)  # Выведет 25
     assert str(phone1) == 'iPhone 14'
     assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
     assert phone1.number_of_sim == 2
---
+++ src/phone.py
@@ -0,0 +1,38 @@
+
+from src.item import Item
+
+class Phone(Item):
+    def __init__(self, name, price, quantity, sim_slots):
+        super().__init__(name, price, quantity)
+        self.number_of_sim = sim_slots
+
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if isinstance(value, int) and value > 0:
+            self._number_of_sim = value
+        else:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+
+    def __repr__(self):
+        return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if isinstance(other, Phone):
+            return Phone(self.name, self.price, self.quantity + other.quantity, self.number_of_sim)
+        elif isinstance(other, Item):
+            return Item(self.name, self.price, self.quantity + other.quantity)
+        else:
+            raise ValueError("Cannot add Phone with non-Phone or Item object")
+
+    def __radd__(self, other):
+        if isinstance(other, Item):
+            return Item(self.name, self.price, self.quantity + other.quantity)
+        else:
+            raise ValueError("Cannot add Phone with non-Item object")
